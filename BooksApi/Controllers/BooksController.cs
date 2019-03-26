67c6165e1ee3e0c42537272e2f7f669872db7b78
Danilo Bojovic <Danilo.Bojovic@seavus.com>
2019-03-26 13:03:06 +0100

    First commit

---
+++ BooksApi/Controllers/BooksController.cs
@@ -0,0 +1,41 @@
+﻿using BooksApi.Models;
+using BooksApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookService _bookService;
+
+        public BooksController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Book>> Get()
+        {
+            return _bookService.Get();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> Get(string id)
+        {
+            var book = _bookService.Get(id);
+
+            if(book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+    }
+}
---
+++ BooksApi/Services/BookService.cs
@@ -0,0 +1,31 @@
+﻿using BooksApi.Models;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksApi.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _books;
+        public BookService(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetConnectionString("BooksDb"));
+            var database = client.GetDatabase("BooksDb");
+            _books = database.GetCollection<Book>("Books");
+        }
+
+        public List<Book> Get()
+        {
+            return _books.Find(book => true).ToList();
+        }
+
+        public Book Get(string id)
+        {
+            return _books.Find(book => book.Id == id).FirstOrDefault();
+        }
+    }
+}
